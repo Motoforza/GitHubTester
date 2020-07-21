1a4d0e8123a499a6ff183b352677e1c4d7db6b74
rkreifeldt <rick@ideabench.com>
2020-07-21 17:32:44 -0600

    Initial Commit

---
+++ GitHubTester/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GitHubTester
+//
+//  Created by Rick Kreifeldt on 7/21/20.
+//  Copyright © 2020 ideaBench. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
